c92dfd5c0170d103199500e6180f7f926dc43a8f
Leah Klamkin & Lisa Duschek <student@epicodus.com>
2015-09-09 11:38:57 -0700

    It will cut the first two consonants of a word.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 var pigLatin = function(word) {
-  var wordAy = word.concat("ay");
-  return wordAy;
+  var vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.indexOf((word.charAt(0))) >= 0) {
+    var wordAy = word.concat("ay");
+    return wordAy;
+} else if (vowels.indexOf((word.charAt(0))) < 0 && vowels.indexOf((word.charAt(1))) < 0) {
+    return word.slice(2);
+} else if (vowels.indexOf((word.charAt(0))) < 0) {
+    return word.slice(1);
+  }
 };
+
+
+
+
+// else {
+//   var wordToArray = word.split("");
+//   var letters = [];
+//   wordToArray.forEach(function(letter) {
+//     do  {
+//       letters.push(letter);
+//     }
+//     while ((vowels.indexOf(letter)) < 0);
+//   });
+//   return letters.join();
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,23 @@
-describe('pigLatin', function() {
-  it("returns the original word", function() {
-    expect(pigLatin("apple")).to.equal("apple");
-  });
-});
+// describe('pigLatin', function() {
+//   it("returns the original word", function() {
+//     expect(pigLatin("apple")).to.equal("apple");
+//   });
+// });
 
 describe('pigLatin', function() {
   it("adds ay to the end of the word", function() {
     expect(pigLatin("apple")).to.equal("appleay");
-  })
-})
+  });
+});
+
+describe('pigLatin', function() {
+  it("adds ay to the end of the word only if the word starts with a vowel", function() {
+    expect(pigLatin("banana")).to.equal("anana");
+  });
+});
+
+describe('pigLatin', function() {
+  it("cuts the first consecutive consonants of a word", function() {
+    expect(pigLatin("cream")).to.equal("eam");
+  });
+});
